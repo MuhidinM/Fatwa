dcdc502a228007d120ac68dcbc4a69849f4475b2
MuhidinM <misbahmuhidin3@gmail.com>
2024-02-29 16:35:59 +0300

    table started

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Payment, columns } from "@/components/dashboard/column";
+import { DataTable } from "@/components/ui/data-table";
 
-const DashboardHome = () => {
-  return <div className="">Dashboard Home</div>;
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const DashboardHome = async () => {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 };
 
 export default DashboardHome;
--- components/logo.tsx
+++ components/logo.tsx
@@ -8,7 +8,7 @@ const Logo = ({ href, small }: { href: string; small?: boolean }) => {
     <div>
       <Link
         href={href}
-        className={cn("flex items-center w-10", small && "w-8 justify-center")}
+        className={cn("flex items-center w-10", small && "w-8 justify-center", !small&& "pl-2")}
       >
         {/* <Image
           src="/logo.png"
@@ -24,9 +24,9 @@ const Logo = ({ href, small }: { href: string; small?: boolean }) => {
           height={40}
           alt="img"
         /> */}
-        <h1 className="font-bold text-2xl">M</h1>
+        <h1 className="font-bold text-2xl">F</h1>
         <span className={cn("font-semibold text-2xl", small && "hidden")}>
-          Logo
+          atwa
         </span>
       </Link>
     </div>
