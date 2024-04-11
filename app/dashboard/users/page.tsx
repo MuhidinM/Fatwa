458131383c54c5c19801f23d8ff6d5970835a277
MuhidinM <misbahmuhidin3@gmail.com>
2024-04-11 16:22:29 +0300

    users view is done

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,6 +1,37 @@
+"use client";
+import { db } from "@/app/firebase-config";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "@/components/users/column";
+import { User } from "@/types/types";
+import { onValue, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const UsersHome = () => {
-    return <div className="md:container mx-auto py-10">Users</div>;
-  };
-  
-  export default UsersHome;
-  
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const usersRef = ref(db, "Users");
+    onValue(usersRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const usersArray: User[] = Object.values(data);
+        const filteredData: User[] = usersArray.filter((user) => user.name);
+        const transformedUsers: User[] = filteredData.map((users) => ({
+          uuid: users.uuid,
+          name: users.name,
+          photo: users.photo,
+          email: users.email,
+          // Add any other transformations you need here
+        }));
+        setUsers(transformedUsers);
+      }
+    });
+  }, []);
+  return (
+    <div className="md:container mx-auto py-10">
+      <DataTable columns={columns} data={users} />
+    </div>
+  );
+};
+
+export default UsersHome;
--- components/users/column.tsx
+++ components/users/column.tsx
@@ -8,6 +8,7 @@ import { DataTableColumnHeader } from "../ui/data-column-header";
 import { DialogFull } from "../dialog-full";
 import DeleteAlert from "../ui/delete-alert";
 import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -16,9 +17,34 @@ export const columns: ColumnDef<Category>[] = [
   {
     accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Ustazs" />
+      <DataTableColumnHeader column={column} title="Users" />
     ),
     cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "photo",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Avatar" />
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">
+        <Avatar>
+          {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+          <AvatarFallback>
+            {(row.getValue("name") as string).slice(0, 2)}
+          </AvatarFallback>
+        </Avatar>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Email" />
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("email")}</div>
+    ),
   },
   {
     accessorKey: "action",
