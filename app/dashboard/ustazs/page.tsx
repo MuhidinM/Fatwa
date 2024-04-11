917d2054c33fd88585f52a93647eabbfa3cc1f5d
MuhidinM <misbahmuhidin3@gmail.com>
2024-04-11 13:08:25 +0300

    view for Ustazs

--- app/dashboard/ustazs/page.tsx
+++ app/dashboard/ustazs/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+
+import { db } from "@/app/firebase-config";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "@/components/ustazs/column";
+import { Teacher } from "@/types/types";
+import { onValue, ref } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const UstazsHome = () => {
-    return <div className="md:container mx-auto py-10">Ustazs</div>;
-  };
-  
-  export default UstazsHome;
-  
+  const [teacher, setTeacher] = useState<Teacher[]>([]);
+
+  useEffect(() => {
+    const teacherRef = ref(db, "ustazs");
+    onValue(teacherRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const teacherArray: Teacher[] = Object.values(data);
+        const transformedTeachers: Teacher[] = teacherArray.map((teacher) => ({
+          uuid: teacher.uuid,
+          name: teacher.name,
+          // Add any other transformations you need here
+        }));
+        setTeacher(transformedTeachers);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="md:container mx-auto py-10">
+      <DataTable columns={columns} data={teacher} />
+    </div>
+  );
+};
+
+export default UstazsHome;
