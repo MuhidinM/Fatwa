d7974fb52a48786bae0c889d1689514925804c9b
MuhidinM <misbahmuhidin3@gmail.com>
2024-06-02 00:41:05 +0300

    don't show any thing unless user is logged in. not even for a sec.😊

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -21,13 +21,17 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   console.log(userLoggedIn);
   return (
     <div className="">
-      <Navbar click={changeSize} small={small} />
-      <div className={cn("p-4 md:ml-64", small && "md:ml-20")}>
-        {/* <Breadcrumbs /> */}
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-          {children}
-        </div>
-      </div>
+      {userLoggedIn && (
+        <>
+          <Navbar click={changeSize} small={small} />
+          <div className={cn("p-4 md:ml-64", small && "md:ml-20")}>
+            {/* <Breadcrumbs /> */}
+            <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+              {children}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
