0bc4565c9d8c3d6623b6da17521783b22e7c65da
MuhidinM <misbahmuhidin3@gmail.com>
2024-06-15 17:58:50 +0300

    hero done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { SparklesPreview } from "@/components/home/hero";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
       {/* <h1>Landing Page</h1> <Link href={"/auth"}>LogIn</Link> */}
+      <SparklesPreview />
     </div>
   );
 }
