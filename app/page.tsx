ce21d375e762e8d3d3f2d0070ec5a1a712e190fa
MuhidinM <misbahmuhidin3@gmail.com>
2024-06-15 18:12:14 +0300

    features page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { SparklesPreview } from "@/components/home/hero";
+import { Features } from "@/components/home/features";
+import { Hero } from "@/components/home/hero";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="px-48 bg-black">
       {/* <h1>Landing Page</h1> <Link href={"/auth"}>LogIn</Link> */}
-      <SparklesPreview />
+      <Hero />
+      <Features />
     </div>
   );
 }
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { SparklesCore } from "../ui/sparkles";
 
-export function SparklesPreview() {
+export function Hero() {
   return (
     <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
